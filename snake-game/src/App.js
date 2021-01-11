f7668f4fce89ea4ad029e651061918b9f25a8f15
Sadat Shahriar Bari <sadatshahriar@iut-dhaka.edu>
2021-01-11 05:19:58 -0800

    Snake class implemented and changes in App.js

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from "react";
+import React, { Component ,state} from "react";
+import Snake from './Snake';
 
 class App extends Component {
 
+  state={
+    snakeDots: [
+      [0,0],
+      [2,0]
+    ]
+  }
+
   render() {
+
     return (
       <div className="game-area">
-        <div className="snake-dot" style={{ top: 0, left: 0 }}></div>
-        <div className="snake-dot" style={{ top: 0, left: '2%' }}></div>
-        <div className="snake-dot" style={{ top: 0, left: '4%' }}></div>
+        <Snake snakeDots={this.state.snakeDots}/>
+        
       </div>
 
     );
